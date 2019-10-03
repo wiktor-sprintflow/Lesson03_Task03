24beff827ab095e55c2d873afaef8fa8f8a766ea
Wiktor Kapanowski <wiktor.kapanowski@gmail.com>
2019-10-03 17:33:06 +0200

    1) Simplification of isEven and isOdd methods in MathOperations.java 2) Bug fix in the MathOperationsTest - now the parameter is sent the methods.

--- src/MathOperations.java
+++ src/MathOperations.java
@@ -2,19 +2,11 @@ public class MathOperations {
     final double PI = 3.14159265359;
 
     boolean isEven(int number) {
-        if (number % 2 == 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return (number % 2 == 0);
     }
 
     boolean isOdd(int number) {
-        if (number % 2 != 0) {
-            return true;
-        } else {
-            return false;
-        }
+        return !isEven(number);
     }
 
     double circleField(double r) {
--- src/MathOperationsTest.java
+++ src/MathOperationsTest.java
@@ -3,8 +3,8 @@ public class MathOperationsTest {
         MathOperations mathOperations = new MathOperations();
 
         int testNumber = 20011;
-        System.out.println("Liczba " + testNumber + " jest patrzysta: " + mathOperations.isEven(20011));
-        System.out.println("Liczba " + testNumber + " jest niepatrzysta: " + mathOperations.isOdd(20011));
+        System.out.println("Liczba " + testNumber + " jest patrzysta: " + mathOperations.isEven(testNumber));
+        System.out.println("Liczba " + testNumber + " jest niepatrzysta: " + mathOperations.isOdd(testNumber));
 
         double radius = 12.4;
         System.out.println("Pole koła o promieniu " + radius + " wynosi: " + mathOperations.circleField(radius));
